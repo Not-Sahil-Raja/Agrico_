513e01e74c0df2ba1fb1f592a120558d8aa8a5ed
ChakrabortyShrestha11 <shresthachakraborty12@gmail.com>
2024-02-10 23:11:00 +0530

    about us, contact us added

--- ClientSide/src/Components/AboutUs/AboutUs.jsx
+++ ClientSide/src/Components/AboutUs/AboutUs.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React from 'react'
 
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+  <>
+    {/* <div>AboutUs</div> */}
+   {/* <h2 className='text-yellow-950 text-center text-4xl mt-10 '>About Us</h2>
+   <div className='max-w-[1320px] md:py-[80] py-5 flex mx-auto'>
+    <div className='basis-[45%] mx-1 mx-black'>insert pic</div>
+
+   </div>
+
+   <div className='basis-[55%]'>
+    <h1 className='text-2xl text-right'>This is heading   Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos natus rem animi nihil earum amet ratione? Enim id, impedit eligendi nesciunt vel delectus voluptatum nostrum doloribus dicta quae facilis placeat.</h1>
+   </div>
+    {/* <div className='bg-red-900 basis-[45%] text-white'>hello</div>
+    <div className='bg-lime-800 basis-[55%] text-white'>hi</div>
+   */} 
+   <section className='w-screen mt-12 bg-orange-200 mt-10 grid h-screen place-items-center'>
+    <div className='flex flex-1 justify-start items-center flex-col gap-6'>
+      <h5 className='font-bold text-lime-950 text-4xl'> About Us</h5>
+      <p className=' text-2xl upppercase font-light text-lime-900'> NAME</p>
+      <p className='w-3/5 text-center text-2xl text-teal-950'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis optio reprehenderit nulla ex tenetur quasi debitis dolorem magnam explicabo eveniet sequi quae hic earum eaque accusamus molestiae, eligendi incidunt! Vero.
+      Ipsa, distinctio debitis quas sequi laborum explicabo quaerat officia libero quia, quisquam corporis voluptate est aut! Quis, iste voluptatem omnis quae ipsum magni officiis iusto expedita quod corporis illum? Necessitatibus.</p>
+     
+     <div className='w-1/4 bg-orange-200 flex  justify-center items-center gap-6'> 
+     <img src="https://media.istockphoto.com/id/1326948783/photo/smart-young-farmers-use-tablets-to-manage-their-farms-and-use-technology-to-reduce-work-time.jpg?s=1024x1024&w=is&k=20&c=dz5y9FQB7WjaiM1l4-PA5hao5xmCMNEFe_zDdxQ5pW0=" alt="Img1" />
+     
+     <img src="https://media.istockphoto.com/id/1412751945/photo/female-farmer-is-holding-a-digital-tablet-in-a-farm-field-smart-farming.jpg?s=1024x1024&w=is&k=20&c=k1LtRRckg8p-ZGEoZb2QB15SlqQxx3Itwp1Xd3-FJzY=" alt="Img2" />
+
+     <img src="https://media.istockphoto.com/id/1255872192/photo/smart-farming-with-iot-futuristic-agriculture-concept.jpg?s=1024x1024&w=is&k=20&c=jK8ir1KXl8oaTxVJ-hGvatRqiRSD8ePaI-3a0V2CDl0=" alt="Img3" />
+     
+     
+     </div>
+    </div>
+   </section>
+
+
+
+
+  </>
+  
+
+  )
 }
 
-export default AboutUs;
+export default AboutUs
