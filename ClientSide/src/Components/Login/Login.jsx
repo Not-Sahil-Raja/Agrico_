5e43e7de839c8d7ebf1bf650b8a5dd9339369e83
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-02-10 12:37:08 +0530

    Login_frnt_func

--- ClientSide/src/Components/Login/Login.jsx
+++ ClientSide/src/Components/Login/Login.jsx
@@ -4,21 +4,55 @@ import { MoveRight } from "lucide-react";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(true);
+  const [userName, setUserName] = useState("");
+  const [phoneNo, setPhoneNo] = useState("");
+
+  const loginCheck = (e) => {
+    //need to add the backend code here
+    e.preventDefault();
+    console.log(email, password);
+    setEmail("");
+    setPassword("");
+  };
+  const registerCheck = (e) => {
+    //need to add the backend code here
+    e.preventDefault();
+    console.log(userName, email, phoneNo, password);
+    setUserName("");
+    setEmail("");
+    setPhoneNo("");
+    setPassword("");
+  };
+
   return (
     <>
       <div className=" bg-[#fffff2] w-full h-[100vh] overflow-hidden pt-[9vh] select-none">
         <div className=" w-full flex">
           <div className="  w-1/2 h-[91vh] px-[3vw] py-[5vh] flex flex-col justify-center items-center">
-            <div className=" flex flex-col mb-[20vh]">
-              <span className=" mx-auto">
+            <div className=" flex flex-col mb-[20vh] relative ">
+              <span className=" mx-auto font-Archivo text-[#202020d7]">
                 Largest Farmer's Education Platform
               </span>
-              <span className=" text-5xl">From Seed to Harvest.</span>
+              <span className=" text-5xl z-20 font-Archivo">
+                From Seed to Harvest.
+              </span>
+              <div className=" bg-[#c3ff7f] w-[20vw] h-[5vh] absolute  top-[70%] left-[5vw] z-0"></div>
             </div>
             <div className="flex flex-col gap-[1.2vh]">
-              <span className=" text-[#3f3f3f]  ">Don't Have an Account</span>
-              <span className=" flex gap-2 justify-center items-center border-b border-b-[black] cursor-pointer">
-                Create account <MoveRight className=" w-[1.5vw]" />
+              <span className=" text-[#3f3f3f] font-Montserrat ">
+                Don't Have an Account
+              </span>
+              <span
+                className=" flex gap-2 justify-center items-center border-b border-b-[black] cursor-pointer font-Montserrat font-semibold"
+                onClick={() => setLogin(!login)}
+              >
+                {login ? <>Create account </> : <>Login</>}
+                <MoveRight
+                  className={`w-[1.3vw] transition-all ${
+                    login ? "" : "-rotate-45"
+                  }`}
+                />
               </span>
             </div>
           </div>
@@ -31,18 +65,26 @@ const Login = () => {
                   className=" w-full h-[90vh] object-cover object-center brightness-50 absolute"
                 />
               </div>
-              <div className=" absolute text-[5vw] top-1/2 left-1/2 text-[#ffffffc2] [transform:translate(-50%,-240%)] whitespace-nowrap ">
+
+              <div className=" absolute text-[5vw] top-1/2 left-1/2 text-[#ffffffc2] [transform:translate(-50%,-240%)] whitespace-nowrap  font-Archivo font-semibold">
                 Welcome Back.
               </div>
-              <div className=" z-20 bg-[#ffffffdc] absolute top-1/2 left-1/2 [transform:translate(-50%,-40%)] w-1/2 h-2/3 rounded-md  drop-shadow-md">
-                <form action="">
-                  <div className=" flex flex-col  px-5 py-5 justify-around h-[56vh] ">
+              <div
+                className={` z-20 bg-[#ffffffdc] absolute top-1/2 left-1/2 w-[19vw] h-[45vh] rotate-45 lg:rounded-md font-Montserrat  transition-transform
+                 ${
+                   login
+                     ? " [transform:translate(-50%,-40%)] "
+                     : "[transform:translate(-250%,-40%)] opacity-50 "
+                 }`}
+              >
+                <form onSubmit={loginCheck}>
+                  <div className=" flex flex-col  px-[2vw] py-[3vh] justify-evenly  h-[45vh] ">
                     <div className="  ">
-                      <span className=" text-2xl">Login</span>
+                      <span className=" text-3xl font-semibold">Login</span>
                     </div>
-                    <div className=" h-[30vh]   flex flex-col justify-evenly">
-                      <div className=" w-full flex flex-col  h-1/2 justify-center">
-                        <span className=" text-sm font-medium">Email</span>
+                    <div className=" h-[30vh]   flex flex-col justify-center font-Epilogue ">
+                      <div className=" w-full flex flex-col  h-1/3 justify-center ">
+                        <span className=" text-base font-medium">Email</span>
                         <input
                           type="email"
                           className=" border-b border-[#3f3f3f] focus:outline-none bg-transparent"
@@ -52,8 +94,8 @@ const Login = () => {
                           }}
                         />
                       </div>
-                      <div className=" w-full flex flex-col h-1/2 justify-center">
-                        <span className=" text-sm font-medium">Password</span>
+                      <div className=" w-full flex flex-col h-1/3 justify-center">
+                        <span className=" text-base font-medium">Password</span>
                         <input
                           type="password"
                           className=" border-b border-[#222222] focus:outline-none bg-transparent"
@@ -68,7 +110,78 @@ const Login = () => {
                     <input
                       type="submit"
                       value="Login"
-                      className=" bg-[#000000] text-[#fffff2] py-2 rounded-lg cursor-pointer hover:bg-[#0f110f] transition-all duration-300 ease-in-out hover:shadow-inner focus:outline-none"
+                      className=" bg-[#000000] text-[#fffff2] py-2 rounded cursor-pointer hover:bg-[#0f110f] transition-all duration-300 ease-in-out hover:shadow-inner focus:outline-none"
+                    />
+                  </div>
+                </form>
+              </div>
+
+              <div
+                className={` z-20 bg-[#ffffffdc] absolute top-1/2 left-1/2 w-[19vw] h-[55vh] lg:rounded-md font-Montserrat  transition-transform
+                 ${
+                   login
+                     ? "[transform:translate(250%,-40%)] opacity-50  "
+                     : " [transform:translate(-50%,-40%)]"
+                 }`}
+              >
+                <form onSubmit={registerCheck}>
+                  <div className=" flex flex-col  px-[2vw] py-[2vh] justify-around h-[55vh] ">
+                    <div className=" mb-[2vh]">
+                      <span className=" text-2xl font-semibold">Register</span>
+                    </div>
+                    <div className=" h-[30vh]   flex flex-col justify-evenly font-Epilogue  gap-[1.2vh]">
+                      <div className=" w-full flex flex-col   justify-center ">
+                        <span className=" text-sm font-semibold">
+                          User Name
+                        </span>
+                        <input
+                          type="text"
+                          className=" border-b border-[#3f3f3f] focus:outline-none bg-transparent h-[3vh]"
+                          value={userName}
+                          onChange={(e) => {
+                            setUserName(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className=" w-full flex flex-col  h-[vh] justify-center ">
+                        <span className=" text-sm font-semibold">Email</span>
+                        <input
+                          type="email"
+                          className=" border-b border-[#3f3f3f] focus:outline-none bg-transparent h-[3vh]"
+                          value={email}
+                          onChange={(e) => {
+                            setEmail(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className=" w-full flex flex-col h-[vh] justify-center">
+                        <span className=" text-sm font-semibold">PhoneNo</span>
+                        <input
+                          type="phone"
+                          className=" border-b border-[#222222] focus:outline-none bg-transparent h-[3vh]"
+                          value={phoneNo}
+                          onChange={(e) => {
+                            setPhoneNo(e.target.value);
+                          }}
+                        />
+                      </div>
+                      <div className=" w-full flex flex-col h-[vh] justify-center">
+                        <span className=" text-sm font-semibold">Password</span>
+                        <input
+                          type="password"
+                          className=" border-b border-[#222222] focus:outline-none bg-transparent h-[3vh]"
+                          value={password}
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                        />
+                      </div>
+                    </div>
+
+                    <input
+                      type="submit"
+                      value="Login"
+                      className=" bg-[#000000] text-[#fffff2] py-2 rounded cursor-pointer hover:bg-[#0f110f] transition-all duration-300 ease-in-out hover:shadow-inner focus:outline-none"
                     />
                   </div>
                 </form>
