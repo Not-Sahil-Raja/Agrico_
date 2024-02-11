42d9da3a79d7afeb75ee5cc727174c0dd8954e29
Roushan1512 <roxie4269@gmail.com>
2024-02-11 15:23:35 +0530

    category.name fixed

--- ClientSide/src/Components/Catalogue/Eachcard.jsx
+++ ClientSide/src/Components/Catalogue/Eachcard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-const Eachcard = (props) => {
+const Eachcard = () => {
+  const location = useLocation();
   return (
     <div className=" bg-[#fffff2] w-full h-[100vh] pt-[9vh] select-none">
       <div className="bg-lightYellow h-fit w-[95%] m-auto rounded-3xl border-black  border-[1px] flex-col justify-center items-center">
-        <div className="bg-black h-[8vh] w-[60%] text-white text-center text-5xl mt-2 rounded-3xl mx-auto">
-          Cat Name
+        <div className="bg-black h-[9vh] w-[60%] text-white text-center text-5xl mt-2 rounded-3xl mx-auto">
+          Category {location.state.name}
         </div>
         <br />
         <div className="flex gap-6 h-fit p-6">
