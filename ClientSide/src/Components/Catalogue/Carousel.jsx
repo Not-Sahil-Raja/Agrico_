315a84f255aebf9014d96377be5231e07d31520f
Avirup Ray <avirupraytun@gmail.com>
2024-02-11 13:18:43 +0530

    updated catagory page

--- ClientSide/src/Components/Catalogue/Carousel.jsx
+++ ClientSide/src/Components/Catalogue/Carousel.jsx
@@ -14,8 +14,8 @@ const Carousel = ({ images }) => {
   }, [images.length]);
 
   return (
-    <div className="relative overflow-hidden">
-      <div className="w-full flex">
+    <div className="relative overflow-hidden object-contain">
+      <div className=" w-[70vw] flex object-contain">
         {images.map((image, i) => (
           <Transition
             as="div"
@@ -27,9 +27,13 @@ const Carousel = ({ images }) => {
             leave="transition-opacity duration-1000"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
-            className="w-full flex-shrink-0"
+            className="w-full flex-shrink-0 object-cover"
           >
-            <img src={image} alt={`Slide ${i + 1}`} className="w-full" />
+            <img
+              src={image}
+              alt={`Slide ${i + 1}`}
+              className="w-full object-cover"
+            />
           </Transition>
         ))}
       </div>
