fd83e8fb41ba5f323b434cc86e4c265a1c3f4406
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-03-06 15:45:51 +0530

    header animation added

--- ClientSide/src/Components/Header/Header.jsx
+++ ClientSide/src/Components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { UserRound } from "lucide-react";
 import { Link, NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { userlogout } from "../../Apps/userInfoslice.js";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, MoveUpRight } from "lucide-react";
+import { motion } from "framer-motion";
 // import Agrico_logo from "../../Images/Agrico_logo.png";
 
 const Header = () => {
@@ -14,27 +15,89 @@ const Header = () => {
     console.log("loggged Out");
   };
 
+  const [logoAnim, setLogoAnim] = useState(false);
+
   const userDet = useSelector((state) => state.userInfo.userInfo);
   // console.log(userDet);
   return (
-    <div className="  mt-[.75vh]  px-[3vw] py-[.5vh] absolute w-full flex font-Archivo text-lg text-[#2b1c1c] font-[570] justify-around items-center drop-shadow-md gap-[5vw] z-50">
-      <div className="  w-[10vw] bg-[#fdcb749c] h-[5.8vh] mr-auto rounded-2xl px-[1vw] flex  justify-evenly items-center shadow-inner backdrop-blur-xl ">
-        <Link className=" py-[.5vh]  px-[.35vw] text-center flex-1 ">
-          {/* <img src={Agrico_logo} alt="" /> */}
-          <span className=" text-2xl  font-semibold font-Syne">AgriCo</span>
+    <div className="  bg-[#bdb9a788] backdrop-blur-md px-[3vw] fixed top-0 w-[100vw] flex font-Archivo text-lg text-[#2b1c1c] font-[570] justify-around items-center drop-shadow-md gap-[5vw] z-50">
+      <div className="  w-[10vw]  h-[5.8vh] mr-auto rounded-2xl px-[1vw] flex  justify-evenly items-center  ">
+        <Link className=" py-[.5vh]  px-[.35vw] text-center flex-1 flex  h-full items-center justify-center relative">
+          <motion.div
+            className=" flex flex-col h-full w-full items-center justify-center relative overflow-hidden"
+            onHoverStart={() => setLogoAnim(!logoAnim)}
+            onHoverEnd={() => setLogoAnim(!logoAnim)}
+          >
+            <motion.div
+              className=" flex items-center justify-center absolute w-full h-full top-1/2 left-1/2 [transform:translate(-50%,-50%)]"
+              animate={{
+                transform: logoAnim
+                  ? "translate(-50%,-160%)"
+                  : "translate(-50%,-50%)",
+              }}
+              transition={{ duration: 0.25, ease: "easeInOut" }}
+            >
+              <span className=" font-Montserrat font-semibold opacity-75 text-[white]">
+                AgriCo
+              </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="15"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="4"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-move-up-right opacity-75"
+              >
+                <path d="M13 5H19V11" />
+                <path d="M19 5L5 19" />
+              </svg>
+            </motion.div>
+            <motion.div
+              className=" flex items-center justify-center absolute w-full h-full top-1/2 left-1/2 [transform:translate(-50%,-50%)]"
+              animate={{
+                transform: logoAnim
+                  ? "translate(-50%,-50%)"
+                  : "translate(-50%,160%)",
+              }}
+              transition={{ duration: 0.25, ease: "easeInOut" }}
+            >
+              <span className=" font-Montserrat font-semibold text-[white]">
+                AgriCo
+              </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="15"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                strokeWidth="4"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-move-up-right"
+              >
+                <path d="M13 5H19V11" />
+                <path d="M19 5L5 19" />
+              </svg>
+            </motion.div>
+          </motion.div>
         </Link>
         {/* <div className=" text-lg mr-auto flex-[3] text-center whitespace-nowrap">
           AgriCo
         </div> */}
       </div>
 
-      <div className=" py-[.75vh] bg-[#f8bf5cd7] flex justify-around w-[35vw] backdrop-blur-3xl rounded-lg ">
+      <div className=" py-[.95vh]  flex justify-around w-[35vw]  rounded-lg ">
         <NavLink
           to="/"
           className={({ isActive }) =>
-            ` px-[1.5vw] py-[.35vh] rounded-[0.5vw] transition-all${
+            ` px-[1.5vw] py-[.35vh] rounded-xl transition-all${
               isActive
-                ? "  bg-[#c7923685] shadow-inner"
+                ? "  bg-[#1a181485] text-white shadow-inner"
                 : " text-[#353535] border-transparent hover:backdrop-blur-sm"
             }`
           }
@@ -44,9 +107,9 @@ const Header = () => {
         <NavLink
           to="/catlg"
           className={({ isActive }) =>
-            `  px-[1.5vw] py-[.35vh] rounded-[0.5vw]  ${
+            `  px-[1.5vw] py-[.35vh] rounded-xl  ${
               isActive
-                ? "bg-[#c7923685] shadow-inner"
+                ? "bg-[#1a181485] text-white shadow-inner"
                 : "text-[#353535] border-transparent hover:backdrop-blur-sm"
             }`
           }
@@ -56,9 +119,9 @@ const Header = () => {
         <NavLink
           to="/marketPlace"
           className={({ isActive }) =>
-            `  px-[1.5vw] py-[.35vh] rounded-[0.5vw]  ${
+            `  px-[1.5vw] py-[.35vh] rounded-xl ${
               isActive
-                ? "bg-[#c7923685] shadow-inner"
+                ? "bg-[#1a181485] text-white shadow-inner"
                 : "text-[#353535] border-transparent hover:backdrop-blur-sm"
             }`
           }
