723b42840cb106c2d011cea6fbe7e69f1e8d38e5
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-03-07 11:42:38 +0530

    multer-ENONET Issue

--- Server/src/middlewares/multer.middleware.js
+++ Server/src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "./Public");
+    cb(null, "./Public/tmp");
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
