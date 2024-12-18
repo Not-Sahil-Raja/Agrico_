4f756592037de13e60fdf729390b20d0bc13bc2e
Not-Sahil-Raja <sahilraja2002@gmail.com>
2024-12-18 16:52:54 +0530

    Refactor multer middleware to remove temporary directory configuration

--- Server/src/middlewares/multer.middleware.js
+++ Server/src/middlewares/multer.middleware.js
@@ -1,12 +1,8 @@
 import multer from "multer";
-import fs from "fs";
 import path from "path";
 
 // Ensure the tmp directory exists
 const tmpDir = path.join(process.cwd(), "tmp");
-if (!fs.existsSync(tmpDir)) {
-  fs.mkdirSync(tmpDir, { recursive: true });
-}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
